ed5ccc17899df85460f38b7edcea6c58369ba11a
biblio21 <biblio21@BIBLIO21>
2018-08-15 12:36:42 -0700

    Add project files.

---
+++ Tarea_Proyecto/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Enemy
+    {
+        //Autopropertires
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+        public int movement { get; set; }
+        public int points { get; set; }
+        //Propertires
+        private int _shot;
+        public int shot
+        {
+            get
+            {
+                return _shot;
+            }
+            set
+            {
+                if (value >= 4)
+                {
+                    _shot = value;
+
+                }
+                else if (value < 4)
+                {
+                    Console.WriteLine("+500 POINTS");
+                    Console.WriteLine("Points: " + points + 500);
+                }
+                else if (value == 0)
+                {
+                    Console.WriteLine("You fail");
+                }
+            }
+
+        }
+        public void explosion()
+        {
+            Console.WriteLine("You killed him, He explode");
+        }
+    }
+}
+
---
+++ Tarea_Proyecto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy enemy1 = new Enemy();
+            enemy1.positionX = 2;
+            enemy1.positionY = 4;
+            enemy1.movement = 4;
+            enemy1.shot = 8;
+            if (enemy1.shot < 5)
+            {
+                enemy1.points = 1000;
+            }
+            else if (enemy1.shot < 2)
+            {
+                enemy1.shot = 100;
+            }
+
+            Console.WriteLine("¡¡¡ACABA CON ROBT!!!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Información de Robt:");
+            Console.WriteLine("posición en X:" + enemy1.positionX);
+            Console.WriteLine("posición Y: " + enemy1.positionY);
+            Console.WriteLine("Numero de movimientos: " + enemy1.movement);
+            Console.WriteLine("Tiros: " + enemy1.shot);
+            Console.WriteLine(" ");
+            if (enemy1.points == 1000 || enemy1.points < 1000)
+            {
+                enemy1.explosion();
+                Console.WriteLine("YOU WIN!!!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
